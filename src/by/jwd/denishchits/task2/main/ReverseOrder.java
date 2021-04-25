2f65d244850f80c42e67c3b7e08678c1547d5b91
maxiemar <denishicmihail@gmail.com>
2021-04-26 00:16:37 +0300

    Integrate new output way

--- src/by/jwd/denishchits/task2/main/ReverseOrder.java
+++ src/by/jwd/denishchits/task2/main/ReverseOrder.java
@@ -1,6 +1,8 @@
 package by.jwd.denishchits.task2.main;
 
 import by.jwd.denishchits.service.Printer;
+import by.jwd.denishchits.service.PrinterArgumentException;
+import by.jwd.denishchits.service.arguments.OutputMode;
 import by.jwd.denishchits.task2.util.ArrayUtils;
 import by.jwd.denishchits.service.InvalidArrayException;
 
@@ -11,11 +13,11 @@ public class ReverseOrder {
     public static void main(String[] args) {
         try {
             List<String> argumentsReversedOrder = ArrayUtils.reverseArrayAsList(args);
-            for (String argument : argumentsReversedOrder) {
-                System.out.print(argument + " ");
-            }
+            Printer.printSeveral(argumentsReversedOrder, OutputMode.SINGLE_LINE);
         } catch (InvalidArrayException e) {
             Printer.printErrorMessage("Arguments list does not exist.");
+        } catch (PrinterArgumentException e) {
+            Printer.printErrorMessage(e.getMessage());
         }
     }
 }
